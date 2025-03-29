f7f1ba3818c09c8382262a9310fdbae8c4bc706c
YOnikosYT <nikosvizogalos@gmail.com>
2025-03-29 15:45:28 +0200

    updated scripts

--- code.lua
+++ code.lua
@@ -1,1 +1,16 @@
-coming soon...
+local placeScripts = {
+    [17574618959] = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Memeboiyot/SyntaxRebornOfficial/main/SyntaxReborn'),true))(), -- syntax reborn just a baseplate
+    [placeid] = comingsoon,
+    [placeid] = comingsoon,
+    [placeid] = comingsoon,
+    [placeid] = comingsoon
+}
+
+local currentPlaceId = game.PlaceId
+
+if placeScripts[currentPlaceId] then
+    print("Executing script for Place ID " .. currentPlaceId)
+else
+    print("Not in a valid game. Kicking player.")
+    game:GetService("Players").LocalPlayer:Kick("Not Supported Game.")
+end

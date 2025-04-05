b6bca19f38f5af5324fc44f67e54afa3cb07b100
YOnikosYT <nikosvizogalos@gmail.com>
2025-04-05 11:45:52 +0300

    Update code.lua
    added bluelock rivals no key

--- code.lua
+++ code.lua
@@ -1,6 +1,6 @@
 local placeScripts = {
     [17574618959] = loadstring(game:HttpGet("https://raw.githubusercontent.com/Memeboiyot/SyntaxRebornOfficial/main/SyntaxReborn"))() -- just a baseplate
-    [18668065416] = loadstring(game:HttpGet("https://pastebin.pl/view/raw/c495deb0"))(), -- blue lock rivals
+    [18668065416] = loadstring(game:HttpGet("https://raw.githubusercontent.com/SubToy0nikos/YoHub/refs/heads/main/bluelockrivalsscript.lua"))(), -- blue lock rivals
     [placeid] = comingsoon,
     [placeid] = comingsoon,
     [placeid] = comingsoon

dde345a563071aed74132f4cd8955b163b3cabe1
YOnikosYT <nikosvizogalos@gmail.com>
2025-07-23 03:12:45 +0300

    Update code.lua

--- code.lua
+++ code.lua
@@ -1,7 +1,8 @@
 local placeScripts = {
     [17574618959] = loadstring(game:HttpGet("https://raw.githubusercontent.com/Memeboiyot/SyntaxRebornOfficial/main/SyntaxReborn"))(), -- just a baseplate
     [18668065416] = loadstring(game:HttpGet("https://raw.githubusercontent.com/SubToy0nikos/YoHub/refs/heads/main/bluelockrivalsscript.lua"))(), -- blue lock rivals
-    [placeid] = comingsoon,
+    [126884695634066] = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))(), -- gag
+    [16732694052] = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))(), -- fisch
     [placeid] = comingsoon,
     [placeid] = comingsoon
 }

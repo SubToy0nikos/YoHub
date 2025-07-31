c513abc4916bca7e78ec73b88a168ab3bacdd76a
YOnikosYT <nikosvizogalos@gmail.com>
2025-07-31 18:57:14 +0300

    Update growacryptofarm.lua

--- growacryptofarm.lua
+++ growacryptofarm.lua
@@ -1,4 +1,1 @@
-print("Made by @yonikosyt on Youtube")
-print("Made by a femboy")
-print("And Trulyy Gold!)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/TrulyyGold/Grow-a-Crypto-Farm/refs/heads/main/CryptoFarm.lua'))()
+loadstring(game:HttpGet('https://scripts.yonikos.xyz/webdav/scripts/growacryptofarm.lua'))()
